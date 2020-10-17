0a5a339edc81def35225a9ba893542425fc5753e
Michel Attallah <michelattallah123@gmail.com>
2020-10-17 13:26:58 +0200

    modified tokens

--- backend/src/api.py
+++ backend/src/api.py
@@ -13,31 +13,32 @@ CORS(app)
 
 # db_drop_and_create_all()
 
-## ROUTES
+# ROUTES
+
 
 @app.route('/drinks')
-def get_drinks():  
+def get_drinks():
     drinks = Drink.query.all()
     drinks = [drink.short() for drink in drinks]
 
-    return jsonify({'success': True, 'drinks':drinks}), 200
+    return jsonify({'success': True, 'drinks': drinks}), 200
+
 
 @app.route('/drinks-detail')
 @requires_auth('get:drinks-detail')
-def get_drinks_detail(payload):  
+def get_drinks_detail(payload):
     drinks = Drink.query.all()
-    drinks = [drink.long() for drink in drinks] 
-    return jsonify({'success': True, 'drinks':drinks}), 200
+    drinks = [drink.long() for drink in drinks]
+    return jsonify({'success': True, 'drinks': drinks}), 200
 
 
-
-@app.route('/drinks',methods=['POST'])
+@app.route('/drinks', methods=['POST'])
 @requires_auth('post:drinks')
 def create_drinks(payload):
     body = request.get_json()
     recipe = body.get('recipe', None)
     title = body.get('title', None)
-    drink = Drink(title=title,recipe=json.dumps(recipe))
+    drink = Drink(title=title, recipe=json.dumps(recipe))
     Drink.insert(drink)
     return jsonify({
         'success': True,
@@ -45,54 +46,52 @@ def create_drinks(payload):
     }), 201
 
 
-
 @app.route('/drinks/<id>', methods=['PATCH'])
 @requires_auth('patch:drinks')
-def edit_drink(payload,id):
-        data = request.get_json()
-        title = data.get('title', None)
-        recipe = data.get('recipe', None)
-        drink = Drink.query.filter_by(id=id).one_or_none()
-        if drink is None:
-            abort(404)
-        drink.title = title
-        drink.recipe = json.dumps(recipe)
-        drink.update()
-        drink = Drink.query.filter_by(id=id).one_or_none()
-        return jsonify({
-            'success': True,
-            'drinks': [drink.long()]
-        })
-
-
-    
+def edit_drink(payload, id):
+    data = request.get_json()
+    title = data.get('title', None)
+    recipe = data.get('recipe', None)
+    drink = Drink.query.filter_by(id=id).one_or_none()
+    if drink is None:
+        abort(404)
+    drink.title = title
+    drink.recipe = json.dumps(recipe)
+    drink.update()
+    drink = Drink.query.filter_by(id=id).one_or_none()
+    return jsonify({
+        'success': True,
+        'drinks': [drink.long()]
+    })
 
 
 @app.route('/drinks/<int:id>', methods=['DELETE'])
 @requires_auth('delete:drinks')
 def delete_drinks(payload, id):
-        drink = Drink.query.filter_by(id=id).one_or_none()
-        if drink is None:
-            abort(404)
-        drink.delete()
-        return jsonify({
-            'success': True,
-            'deleted': drink_id
-        })
+    drink = Drink.query.filter_by(id=id).one_or_none()
+    if drink is None:
+        abort(404)
+    drink.delete()
+    return jsonify({
+        'success': True,
+        'deleted': drink_id
+    })
 
 
-
-## Error Handling
+# Error Handling
 '''
 Example error handling for unprocessable entity
 '''
+
+
 @app.errorhandler(422)
 def unprocessable(error):
     return jsonify({
-                    "success": False, 
-                    "error": 422,
-                    "message": "Unprocessable"
-                    }), 422
+        "success": False,
+        "error": 422,
+        "message": "Unprocessable"
+    }), 422
+
 
 @app.errorhandler(404)
 def not_found(error):
@@ -100,8 +99,9 @@ def not_found(error):
         "success": False,
         "error": 404,
         "message": "Resource not found"
-    
+
     }), 404
+
 
 @app.errorhandler(401)
 def unauthorized(error):
@@ -109,7 +109,8 @@ def unauthorized(error):
         'success': False,
         'error': 401,
         'message': 'Unauthorized'
-    } , 401)
+    }, 401)
+
 
 @app.errorhandler(405)
 def method_not_allowed(error):
@@ -118,6 +119,7 @@ def method_not_allowed(error):
         'error': 405,
         'message': 'Method not allowed'
     }, 405)
+
 
 @app.errorhandler(500)
 def internal_server_error(error):
